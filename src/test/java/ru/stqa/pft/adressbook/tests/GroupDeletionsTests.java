19325ad7e07a20abf154349406c5e5f232cfe46e
Marina Astrecova <yamshikova@list.ru>
2021-04-09 15:44:03 +0300

    Лекция 5.4. Fluent-интерфейсы

--- src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionsTests.java
+++ src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionsTests.java
@@ -12,7 +12,7 @@ public class GroupDeletionsTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0) {
-      app.group().create(new GroupData("testik", null, null));
+      app.group().create(new GroupData().withName("test1"));
     }
   }
 
