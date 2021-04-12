fb0eb35c8c9df3e3f3b40f3ac1ba7aee7a0d712a
Marina Astrecova <yamshikova@list.ru>
2021-04-12 11:16:51 +0300

    Задание №9: правки при  модификации контакта

--- src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTest.java
+++ src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTest.java
@@ -12,7 +12,7 @@ public class ContactModificationTest extends TestBase {
     @Test
     public void testContactModification() {
         app.goToPage().home();
-        if (app.group().list().size() == 0) {
+        if (app.contact().list().size() == 0) {
             app.contact().createContact(new ContactData()
                     .withFirstname("111").withLastname("222").withMobile("333").withEmail("444").withEmail2("555")
                     .withAddress("66").withAddress2("777").withByear("888"));
@@ -20,7 +20,7 @@ public class ContactModificationTest extends TestBase {
         List<ContactData> before = app.contact().list();
         app.contact().editContactModification(before.size() - 1);
         int index = before.size() - 1;
-        ContactData contact = new ContactData().withId(before.get(index).getId()).withFirstname("111").withLastname("xxx")
+        ContactData contact = new ContactData().withId(before.get(index).getId()).withFirstname("100").withLastname("200")
                 .withMobile("333").withEmail("444").withEmail2("555")
                 .withAddress("123").withAddress2("777").withByear("888");
         app.contact().fillContactForm(contact);
